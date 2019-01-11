e373a16f27a1617e2890c989df9a5d93c46c1ead
Janujan <janujan.selva@gmail.com>
2019-01-11 03:03:31 -0500

    implementing update and complete tests

--- commerce/tests.py
+++ commerce/tests.py
@@ -17,6 +17,11 @@ class BaseViewTest(APITestCase):
     def create_Item(title, price, quantity ):
         if title != "" and price != 0 and quantity !=0:
             Item.objects.create(title=title, price=price, inventory_count=quantity)
+    @staticmethod
+    def create_Cart(name, total_val, status ):
+        Cart.objects.create(cart_name=name, total_val=total_val,
+            cart_status=status)
+
 
     def setUp(self):
         # add test data
@@ -26,6 +31,7 @@ class BaseViewTest(APITestCase):
         self.create_Item('erasor', 10.99, 19)
         self.create_Item('glue', 20.99, 0)
         self.create_Item('tape', 3.99, 0)
+        self.create_Cart('test', 0, 0)
 
 
 class GetItemsTest(BaseViewTest):
@@ -72,6 +78,56 @@ class PurchasePostTest(BaseViewTest):
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(ItemOrder.objects.count(), 1)
         self.assertEqual(ItemOrder.objects.get().title, 'pen')
+
+
+class PurchaseCartTest(BaseViewTest):
+    def test_create_Cart(self):
+        """
+        Ensure we can create a new account object.
+        """
+        url = reverse('commerce:itemlist', kwargs={'version':'v2'})
+        new_cart = Cart.objects.all()
+        new_cart.delete()
+
+        data = {'command':'create', 'cart_name':'tests'}
+        response = self.client.post(url, data, format='json')
+
+        json_response = response.json()
+        cart_id = int(json_response['cart_id'])
+
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Cart.objects.count(), 1)
+        self.assertEqual(Cart.objects.get().cart_id, cart_id)
+
+    def test_update_Cart(self):
+        """
+        Ensure we can update a cart object.
+        """
+        url = reverse('commerce:itemlist', kwargs={'version':'v2'})
+        data = {'command':'update', 'cart_id':1, 'title':'pencil', 'quantity':5 }
+        response = self.client.post(url, data, format='json')
+
+        json_response = response.json()
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(ItemOrder.objects.count(), 1)
+        self.assertEqual(ItemOrder.objects.get().title, 'pencil')
+
+    def test_complete_Cart(self):
+        """
+        Ensure we can update a cart object.
+        """
+        url = reverse('commerce:itemlist', kwargs={'version':'v2'})
+
+        data = {'command':'complete', 'cart_id':1 }
+        response = self.client.post(url, data, format='json')
+
+        json_response = response.json()
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Cart.objects.count(), 1)
+        self.assertEqual(Cart.objects.get().cart_status, 1)
 
 # class PlayerPostTestFail(APITestCase):
 #     def test_fail_create_Player(self):
