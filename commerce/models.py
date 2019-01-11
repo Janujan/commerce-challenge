a2ca53f28416508c738cbd05688148eaad14f5b4
Janujan <janujan.selva@gmail.com>
2019-01-11 18:29:15 -0500

    changing to PostGreSQL

--- commerce/models.py
+++ commerce/models.py
@@ -6,14 +6,11 @@ class Item(models.Model):
     title = models.CharField(max_length=200)
     price = models.FloatField( default = 0)
     inventory_count = models.IntegerField( default = 0)
-
-
 class Cart(models.Model):
     cart_id = models.AutoField(primary_key=True, max_length=30)
     total_val = models.FloatField(default= 0)
     cart_name = models.CharField(max_length=200)
     cart_status = models.BooleanField(default=False)
-    
 class ItemOrder(models.Model):
     cart = models.ForeignKey(Cart, related_name= 'items', null=True, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
